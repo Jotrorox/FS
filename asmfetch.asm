4ba37480580f7c3941004ae15ad3d8e6c54037c9
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-10-16 20:06:57 +0200

    Enhance asmfetch.asm to improve system info retrieval; add CPU model extraction and refine output formatting

--- asmfetch.asm
+++ asmfetch.asm
@@ -1,4 +1,4 @@
-; Fixed system fetch utility
+; Fixed system fetch utility with proper system info reading
 ; Compile with: nasm -f elf64 asmfetch.asm
 ; Link with: ld asmfetch.o -o asmfetch
 
@@ -15,7 +15,7 @@ section .data
     ascii_art_len equ $ - ascii_art
 
     ; Messages
-    os_msg db "OS: ", 0
+    os_msg db "OS: Linux ", 0
     os_msg_len equ $ - os_msg
     
     kernel_msg db "Kernel: ", 0
@@ -24,21 +24,20 @@ section .data
     cpu_msg db "CPU: ", 0
     cpu_msg_len equ $ - cpu_msg
     
-    ram_msg db "RAM: ", 0
-    ram_msg_len equ $ - ram_msg
+    model_name db "model name", 0
+    model_name_len equ $ - model_name
     
-    newline db 10, 0
-    separator db "-----------------", 10, 0
+    newline db 10
+    separator db "-----------------", 10
     separator_len equ $ - separator
 
     ; File paths
     cpuinfo_path db "/proc/cpuinfo", 0
-    meminfo_path db "/proc/meminfo", 0
 
 section .bss
-    hostname resb 64
     buffer resb 4096
-    uname_buf resb 65
+    cpu_buffer resb 1024
+    hostname resb 64
 
 section .text
     global _start
@@ -75,7 +74,8 @@ _start:
     mov rax, 1
     mov rdi, 1
     mov rsi, hostname
-    mov rdx, 64
+    call get_string_length
+    mov rdx, rax
     syscall
 
     ; Print newline
@@ -85,43 +85,15 @@ _start:
     mov rdx, 1
     syscall
 
-    ; Print CPU info message
+    ; Print CPU message
     mov rax, 1
     mov rdi, 1
     mov rsi, cpu_msg
     mov rdx, cpu_msg_len
     syscall
 
-    ; Open /proc/cpuinfo
-    mov rax, 2          ; sys_open
-    mov rdi, cpuinfo_path
-    mov rsi, 0          ; O_RDONLY
-    mov rdx, 0
-    syscall
-
-    ; Read CPU info
-    mov rdi, rax        ; file descriptor
-    mov rax, 0          ; sys_read
-    mov rsi, buffer
-    mov rdx, 4096
-    syscall
-
-    ; Close CPU info file
-    mov rax, 3          ; sys_close
-    syscall
-
-    ; Find the model name line
-    mov rsi, buffer
-    mov rdi, buffer
-    call find_model_name
-    
-    ; Print CPU model
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, buffer
-    call strlen
-    mov rdx, rax
-    syscall
+    ; Get CPU info
+    call get_cpu_info
 
     ; Print newline
     mov rax, 1
@@ -135,59 +107,142 @@ _start:
     xor rdi, rdi
     syscall
 
-; Find CPU model name in /proc/cpuinfo
-find_model_name:
-    mov rcx, 4096       ; maximum length to search
-.loop:
-    cmp byte [rsi], 0
-    je .done
-    
-    ; Check if current line starts with "model name"
-    cmp dword [rsi], 0x646F6D    ; "mod"
-    jne .next_line
-    
-    cmp dword [rsi + 4], 0x6C65  ; "el "
-    jne .next_line
-    
-    ; Found model name line, skip to ":"
-.find_colon:
-    inc rsi
-    cmp byte [rsi], ':'
-    jne .find_colon
-    
-    ; Skip ": " and copy the rest to buffer
-    add rsi, 2
-    mov rdi, buffer
-.copy_model:
-    mov al, [rsi]
-    mov [rdi], al
-    inc rsi
-    inc rdi
-    cmp al, 10          ; newline
-    je .terminate
-    jmp .copy_model
+; Function to get CPU info
+get_cpu_info:
+    ; Open /proc/cpuinfo
+    mov rax, 2          ; sys_open
+    mov rdi, cpuinfo_path
+    mov rsi, 0          ; O_RDONLY
+    mov rdx, 0
+    syscall
 
-.next_line:
-    inc rsi
-    dec rcx
-    jnz .loop
+    ; Check for error
+    cmp rax, 0
+    jl .error
 
-.terminate:
-    mov byte [rdi - 1], 0    ; null terminate
-.done:
+    ; Save file descriptor
+    mov r8, rax
+
+    ; Read file content
+    mov rax, 0          ; sys_read
+    mov rdi, r8
+    mov rsi, buffer
+    mov rdx, 4096
+    syscall
+
+    ; Close file
+    push rax            ; save number of bytes read
+    mov rax, 3          ; sys_close
+    mov rdi, r8
+    syscall
+    pop rax
+
+    ; Find CPU model name
+    mov rdi, buffer     ; source buffer
+    mov rsi, cpu_buffer ; destination buffer
+    call find_cpu_model
+
+    ; Print CPU model
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, cpu_buffer
+    call get_string_length
+    mov rdx, rax
+    syscall
+
     ret
 
-; Calculate string length
-strlen:
+.error:
+    ; Handle error (just return for now)
+    ret
+
+; Function to find CPU model name in buffer
+find_cpu_model:
+    push rbx
+    mov rbx, rdi        ; save buffer address
+    xor rcx, rcx        ; counter
+
+.search_loop:
+    ; Check for end of buffer
+    cmp byte [rbx + rcx], 0
+    je .not_found
+
+    ; Check if current position matches "model name"
+    mov rdi, rbx
+    add rdi, rcx
+    mov rsi, model_name
+    mov rdx, model_name_len
+    call compare_strings
+    test rax, rax
+    jz .found_model
+
+    inc rcx
+    jmp .search_loop
+
+.found_model:
+    ; Skip to ':'
+    add rcx, model_name_len
+.find_colon:
+    cmp byte [rbx + rcx], ':'
+    je .copy_value
+    inc rcx
+    jmp .find_colon
+
+.copy_value:
+    ; Skip ':' and spaces
+    inc rcx
+    cmp byte [rbx + rcx], ' '
+    je .copy_value
+
+    ; Copy value to cpu_buffer
+    mov rdi, cpu_buffer
+.copy_loop:
+    mov al, [rbx + rcx]
+    cmp al, 10          ; newline
+    je .done
+    mov [rdi], al
+    inc rcx
+    inc rdi
+    jmp .copy_loop
+
+.done:
+    mov byte [rdi], 0   ; null terminate
+    pop rbx
+    ret
+
+.not_found:
+    mov byte [cpu_buffer], 0
+    pop rbx
+    ret
+
+; Function to compare strings
+compare_strings:
+    push rcx
+    push rsi
+    push rdi
+    mov rcx, rdx
+    repe cmpsb
+    setz al
+    movzx rax, al
+    pop rdi
+    pop rsi
+    pop rcx
+    ret
+
+; Function to get string length
+get_string_length:
+    push rcx
     push rdi
     mov rdi, rsi
     xor rcx, rcx
-    not rcx
-    xor al, al
-    cld
-    repne scasb
-    not rcx
-    dec rcx
+.loop:
+    cmp byte [rdi], 0
+    je .done
+    inc rcx
+    inc rdi
+    jmp .loop
+.done:
     mov rax, rcx
     pop rdi
+    pop rcx
     ret
