3dbc338fc2f2d6d0f492defc4059f2fac76d6292
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-10-16 20:05:58 +0200

    Refactor asmfetch.asm to remove color codes and improve structure; streamline output and enhance readability

--- asmfetch.asm
+++ asmfetch.asm
@@ -1,241 +1,193 @@
-; Enhanced system fetch utility with ASCII art and colors
-; Compile with: nasm -f elf64 sysfetch.asm
-; Link with: ld sysfetch.o -o sysfetch
-; Run with: ./sysfetch
+; Fixed system fetch utility
+; Compile with: nasm -f elf64 asmfetch.asm
+; Link with: ld asmfetch.o -o asmfetch
 
 section .data
-    ; ANSI color codes
-    color_reset db 27, "[0m", 0
-    color_blue db 27, "[34m", 0
-    color_green db 27, "[32m", 0
-    color_yellow db 27, "[33m", 0
-    
-    ; ASCII art (penguin)
-    ascii_art db 27, "[33m",  "    .--.   ", 10
-             db "   |o_o |  ", 10
-             db "   |:_/ |  ", 10
-             db "  //   \ \ ", 10
-             db " (|     | )", 10
-             db "/'\_   _/`\", 10
-             db "\___)=(___/", 10, 27, "[0m", 0
-             
+    ; ASCII art
+    ascii_art:
+        db "    .--.   ", 10
+        db "   |o_o |  ", 10
+        db "   |:_/ |  ", 10
+        db "  //   \ \ ", 10
+        db " (|     | )", 10
+        db "/'\_   _/`\", 10
+        db "\___)=(___/", 10, 0
+    ascii_art_len equ $ - ascii_art
+
     ; Messages
-    title_fmt db 27, "[1;34m%s@%s", 27, "[0m", 10, 0
-    separator db 27, "[34m", "-----------------", 27, "[0m", 10, 0
-    hostname_fmt db 27, "[32mOS: ", 27, "[0m%s", 10, 0
-    kernel_fmt db 27, "[32mKernel: ", 27, "[0m%s", 0
-    cpu_msg db 27, "[32mCPU: ", 27, "[0m", 0
-    mem_msg db 27, "[32mMemory: ", 27, "[0m", 0
-    uptime_msg db 27, "[32mUptime: ", 27, "[0m", 0
-    shell_msg db 27, "[32mShell: ", 27, "[0m", 0
+    os_msg db "OS: ", 0
+    os_msg_len equ $ - os_msg
     
+    kernel_msg db "Kernel: ", 0
+    kernel_msg_len equ $ - kernel_msg
+    
+    cpu_msg db "CPU: ", 0
+    cpu_msg_len equ $ - cpu_msg
+    
+    ram_msg db "RAM: ", 0
+    ram_msg_len equ $ - ram_msg
+    
+    newline db 10, 0
+    separator db "-----------------", 10, 0
+    separator_len equ $ - separator
+
     ; File paths
     cpuinfo_path db "/proc/cpuinfo", 0
     meminfo_path db "/proc/meminfo", 0
-    uptime_path db "/proc/uptime", 0
-    
-    ; Buffers
-    buffer times 4096 db 0
-    hostname_buf times 64 db 0
-    username_buf times 64 db 0
-    
+
 section .bss
-    fd_out resq 1
-    stat_buf resb 144
-    
+    hostname resb 64
+    buffer resb 4096
+    uname_buf resb 65
+
 section .text
     global _start
-    
+
 _start:
     ; Print ASCII art
+    mov rax, 1          ; sys_write
+    mov rdi, 1          ; stdout
+    mov rsi, ascii_art
+    mov rdx, ascii_art_len
+    syscall
+
+    ; Print separator
     mov rax, 1
     mov rdi, 1
-    mov rsi, ascii_art
-    mov rdx, 150
+    mov rsi, separator
+    mov rdx, separator_len
     syscall
-    
-    ; Get username (from env or getuid)
-    mov rax, 102        ; sys_getuid
-    syscall
-    
+
     ; Get hostname
     mov rax, 65         ; sys_gethostname
-    mov rdi, hostname_buf
+    mov rdi, hostname
     mov rsi, 64
     syscall
-    
-    ; Print title (username@hostname)
-    mov rdi, title_fmt
-    mov rsi, username_buf
-    mov rdx, hostname_buf
-    call printf
-    
-    ; Print separator
-    mov rdi, separator
-    call printf
-    
-    ; Read and print CPU info
-    mov rdi, cpu_msg
-    call printf
-    
+
+    ; Print OS message
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, os_msg
+    mov rdx, os_msg_len
+    syscall
+
+    ; Print hostname
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, hostname
+    mov rdx, 64
+    syscall
+
+    ; Print newline
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+
+    ; Print CPU info message
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, cpu_msg
+    mov rdx, cpu_msg_len
+    syscall
+
     ; Open /proc/cpuinfo
     mov rax, 2          ; sys_open
     mov rdi, cpuinfo_path
-    xor rsi, rsi        ; O_RDONLY
+    mov rsi, 0          ; O_RDONLY
+    mov rdx, 0
     syscall
-    
-    ; Read CPU model name
-    mov rdi, rax
+
+    ; Read CPU info
+    mov rdi, rax        ; file descriptor
+    mov rax, 0          ; sys_read
     mov rsi, buffer
     mov rdx, 4096
-    call read_until_newline
-    
-    ; Print CPU info
-    mov rdi, buffer
-    call printf
-    mov rdi, 10
-    call putchar
-    
-    ; Read and print memory info
-    mov rdi, mem_msg
-    call printf
-    
-    ; Open /proc/meminfo
-    mov rax, 2
-    mov rdi, meminfo_path
-    xor rsi, rsi
     syscall
-    
-    ; Read memory info
-    mov rdi, rax
+
+    ; Close CPU info file
+    mov rax, 3          ; sys_close
+    syscall
+
+    ; Find the model name line
     mov rsi, buffer
-    mov rdx, 4096
-    call read_until_newline
-    
-    ; Print memory info
     mov rdi, buffer
-    call printf
-    mov rdi, 10
-    call putchar
+    call find_model_name
     
-    ; Get and print kernel version
-    mov rdi, kernel_fmt
-    call printf
-    
-    mov rax, 59         ; sys_uname
-    mov rdi, buffer
+    ; Print CPU model
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, buffer
+    call strlen
+    mov rdx, rax
     syscall
-    
-    mov rdi, buffer
-    call printf
-    mov rdi, 10
-    call putchar
-    
-    ; Print shell info
-    mov rdi, shell_msg
-    call printf
-    
-    ; Get SHELL environment variable
-    mov rax, 119        ; sys_getenv
-    mov rdi, "SHELL"
+
+    ; Print newline
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
     syscall
-    
-    mov rdi, rax
-    call printf
-    mov rdi, 10
-    call putchar
-    
+
     ; Exit
-    mov rax, 60
+    mov rax, 60         ; sys_exit
     xor rdi, rdi
     syscall
 
-; Helper functions
-read_until_newline:
-    push rbp
-    mov rbp, rsp
-    
-    ; Save registers
-    push rbx
-    push r12
-    push r13
-    
-    mov rbx, rdi        ; fd
-    mov r12, rsi        ; buffer
-    mov r13, rdx        ; max length
-    
-    xor rcx, rcx        ; byte counter
-    
-.read_loop:
-    mov rax, 0          ; sys_read
-    mov rdi, rbx
-    lea rsi, [r12 + rcx]
-    mov rdx, 1
-    syscall
-    
-    test rax, rax
-    jle .done
-    
-    movzx eax, byte [r12 + rcx]
-    inc rcx
-    
-    cmp al, 10          ; newline
+; Find CPU model name in /proc/cpuinfo
+find_model_name:
+    mov rcx, 4096       ; maximum length to search
+.loop:
+    cmp byte [rsi], 0
     je .done
     
-    cmp rcx, r13
-    jl .read_loop
+    ; Check if current line starts with "model name"
+    cmp dword [rsi], 0x646F6D    ; "mod"
+    jne .next_line
     
-.done:
-    mov byte [r12 + rcx], 0    ; null terminate
+    cmp dword [rsi + 4], 0x6C65  ; "el "
+    jne .next_line
     
-    ; Restore registers
-    pop r13
-    pop r12
-    pop rbx
-    
-    mov rsp, rbp
-    pop rbp
-    ret
-
-printf:
-    ; Simple printf implementation
-    push rbp
-    mov rbp, rsp
-    
-    mov rax, 1          ; sys_write
-    mov rsi, rdi        ; string
-    mov rdi, 1          ; stdout
-    
-    ; Calculate string length
-    push rsi
-    xor rdx, rdx
-.strlen_loop:
-    cmp byte [rsi], 0
-    je .strlen_done
+    ; Found model name line, skip to ":"
+.find_colon:
     inc rsi
-    inc rdx
-    jmp .strlen_loop
-.strlen_done:
-    pop rsi
+    cmp byte [rsi], ':'
+    jne .find_colon
     
-    syscall
-    
-    mov rsp, rbp
-    pop rbp
+    ; Skip ": " and copy the rest to buffer
+    add rsi, 2
+    mov rdi, buffer
+.copy_model:
+    mov al, [rsi]
+    mov [rdi], al
+    inc rsi
+    inc rdi
+    cmp al, 10          ; newline
+    je .terminate
+    jmp .copy_model
+
+.next_line:
+    inc rsi
+    dec rcx
+    jnz .loop
+
+.terminate:
+    mov byte [rdi - 1], 0    ; null terminate
+.done:
     ret
 
-putchar:
-    push rbp
-    mov rbp, rsp
-    
+; Calculate string length
+strlen:
     push rdi
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, rsp
-    mov rdx, 1
-    syscall
+    mov rdi, rsi
+    xor rcx, rcx
+    not rcx
+    xor al, al
+    cld
+    repne scasb
+    not rcx
+    dec rcx
+    mov rax, rcx
     pop rdi
-    
-    mov rsp, rbp
-    pop rbp
     ret
