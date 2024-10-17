d11dd4b125406ab1a66c992c72da5f66f6d65e88
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-10-17 10:29:47 +0200

    Enhance asmfetch.asm with ANSI escape codes for improved ASCII art styling; add color formatting to output

--- asmfetch.asm
+++ asmfetch.asm
@@ -1,12 +1,24 @@
 section .data
+    ; ANSI escape codes
+    esc db 0x1B, '[', 0
+    reset db 0x1B, '[0m', 0
+    bold db 0x1B, '[1m', 0
+    red db 0x1B, '[31m', 0
+    green db 0x1B, '[32m', 0
+    yellow db 0x1B, '[33m', 0
+    blue db 0x1B, '[34m', 0
+    magenta db 0x1B, '[35m', 0
+    cyan db 0x1B, '[36m', 0
+    white db 0x1B, '[37m', 0
+
     ; ASCII art with text
     ascii_art:
         db "           " 10
         db "    .--.   ", 10
-        db "   |o_o |  ", "                 ASMFetch", 10
+        db "   |o_o |  ", 0x1B, '[1m', "                 ASMFetch", 0x1B, '[0m', 10
         db "   |:_/ |  ", 10
-        db "  //   \ \ ", "  A simple fetch program written in asm", 10
-        db " (|     | )", "               - by Jotrorox", 10
+        db "  //   \ \ ", 0x1B, '[32m', "  A simple fetch program written in asm", 0x1B, '[0m', 10
+        db " (|     | )", 0x1B, '[33m', "               - by Jotrorox", 0x1B, '[0m', 10
         db "/'\_   _/`\", 10
         db "\___)=(___/", 10
         db "           ", 10, 0
@@ -26,7 +38,7 @@ section .data
     model_name_len equ $ - model_name
     
     newline db 10
-    separator db "-----------------", 10
+    separator db 0x1B, '[34m', "-----------------", 0x1B, '[0m', 10
     separator_len equ $ - separator
 
 section .bss
