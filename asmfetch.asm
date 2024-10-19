ca5af371a1138cc0ab933ff48cc81e2f8da49f9e
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-10-19 13:45:17 +0200

    Enhance asmfetch.asm to retrieve and display hostname; refactor print and execute functions for improved readability

--- asmfetch.asm
+++ asmfetch.asm
@@ -28,12 +28,16 @@ section .data
     username_msg db 0x1B, '[34m', "Username: ", 0x1B, '[0m', 0
     username_msg_len equ $ - username_msg
 
+    hostname_msg db 0x1B, '[34m', "Hostname: ", 0x1B, '[0m', 0
+    hostname_msg_len equ $ - hostname_msg
+
     newline db 10
     separator db 0x1B, '[34m', "-----------------", 0x1B, '[0m', 10
     separator_len equ $ - separator
 
     ; Commands
     whoami_cmd db "/usr/bin/whoami", 0
+    hostname_cmd db "/bin/hostname", 0
 
 section .bss
     buffer resb 4096
@@ -45,66 +49,90 @@ section .text
 
 _start:
     ; Print ASCII art
-    mov rax, 1          ; sys_write
-    mov rdi, 1          ; stdout
-    mov rsi, ascii_art
-    mov rdx, ascii_art_len
-    syscall
+    mov rdi, ascii_art
+    mov rsi, ascii_art_len
+    call print_string
 
     ; Print separator
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, separator
-    mov rdx, separator_len
-    syscall
-
-    ; Print the username message
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, username_msg
-    mov rdx, username_msg_len
-    syscall
-
-    ; Fork process
-    mov rax, 57         ; sys_fork
-    syscall
-
-    ; Child process
-    cmp rax, 0
-    jz get_username
-
-    ; Parent process waits for child
-    mov rdi, rax        ; child pid
-    mov rax, 3          ; sys_wait4
-    syscall
-
-    ; Exit
-    mov rax, 60         ; sys_exit
-    xor rdi, rdi
-    syscall
-
-get_username:
-    ; Get username
-    mov rax, 59         ; sys_execve
-    mov rdi, whoami_cmd
-    xor rsi, rsi
-    xor rdx, rdx
-    syscall
-
-    ; Read output
-    mov rax, 0          ; sys_read
-    mov rdi, 0          ; stdin
-    mov rsi, buffer
-    mov rdx, 4096
-    syscall
+    mov rdi, separator
+    mov rsi, separator_len
+    call print_string
 
     ; Print username
-    mov rax, 1          ; sys_write
-    mov rdi, 1          ; stdout
-    mov rsi, buffer
-    syscall
+    mov rdi, username_msg
+    mov rsi, username_msg_len
+    call print_string
+
+    mov rdi, whoami_cmd
+    call execute_command
+
+    ; Print hostname
+    mov rdi, hostname_msg
+    mov rsi, hostname_msg_len
+    call print_string
+
+    mov rdi, hostname_cmd
+    call execute_command
 
     ; Exit
     mov rax, 60         ; sys_exit
     xor rdi, rdi
+    syscall
+
+; Function to print a string
+print_string:
+    ; rdi = string address
+    ; rsi = string length
+    push rax
+    push rdi
+    push rsi
+    push rdx
+    
+    mov rax, 1          ; sys_write
+    mov rdx, rsi        ; length
+    mov rsi, rdi        ; string address
+    mov rdi, 1          ; stdout
+    syscall
+    
+    pop rdx
+    pop rsi
+    pop rdi
+    pop rax
+    ret
+
+; Function to execute a command
+execute_command:
+    ; rdi = command address
+    push rax
+    push rsi
+    push rdx
+    
+    mov rax, 57         ; sys_fork
+    syscall
+    
+    cmp rax, 0
+    jz .child_process   ; If we're the child process
+    
+    ; Parent process waits for child
+    push rdi
+    mov rdi, rax        ; PID to wait for
+    mov rax, 61         ; sys_wait4
+    syscall
+    pop rdi
+    
+    pop rdx
+    pop rsi
+    pop rax
+    ret
+
+.child_process:
+    mov rax, 59         ; sys_execve
+    ; rdi already contains command address
+    xor rsi, rsi        ; no arguments
+    xor rdx, rdx        ; no environment variables
+    syscall
+    
+    ; Exit child (only reaches here if execve fails)
+    mov rax, 60         ; sys_exit
+    mov rdi, 1          ; error status
     syscall
